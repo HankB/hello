93f2d30cb1e6db5fa266f3a57c241ccd58df664b
Hank Barta <hbarta@gmail.com>
2022-12-19 17:10:37 -0600

    unit test

---
+++ test/hello_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+	"hello/pkg/lib"
+)
+
+func TestAdd(t *testing.T) {
+	if lib.Add(1,2) != 3 {	// passing test
+		t.Fatal("Add(1,2) did not return 4")
+	}
+	if lib.Add(1,3) != 3 { // failing test
+		t.Fatal("Add(1,3) did not return 3")
+	}
+}
